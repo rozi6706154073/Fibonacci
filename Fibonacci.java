e4352aa61542fcec47b3e3e3b14d97947ef62d1e
Rozi Wardana <mrozin22@gmail.com>
2017-04-05 09:26:11 +0700

    Update Fibonacci.java
    rozi ubah dr i++ ke i--

--- Fibonacci.java
+++ Fibonacci.java
@@ -11,8 +11,8 @@ public class Fibonacci {
         for(int i = 2; i < n; i++) {
             fib[i] = fib[i-1] + fib[i-2];
         }
-         
-        for (int i = 0; i < n; i++) {
+         //i++ jd i--
+        for (int i = 0; i < n; i--) {
             System.out.print(fib[i] +  " ");
         }
     }
